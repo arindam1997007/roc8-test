2b4fb0425817fe0121d7a7539d3555122eed5a3c
Arindam Chowdhury <arindamchy00@gmail.com>
2023-07-16 17:18:04 +0530

    axios installed

--- index.js
+++ index.js
@@ -6,28 +6,39 @@ const apiKey = "4NKQ3-815C2-8T5Q2-16318-55301"
 const url = "https://devcore02.cimet.io/v1/generate-token"
 
 const app = express()
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 3000
 
 // Enable CORS
 app.use(cors({ origin: "*" }))
 
 // Routes
-app.get("/token", (req, res) => {
+app.get("/token", async (req, res) => {
 	console.log("calling")
-	fetch(url, {
-		method: "POST",
-		headers: {
-			"Api-Key": "4NKQ3-815C2-8T5Q2-16318-55301",
-		},
-	})
-		.then(response => response.json())
-		.then(data => {
-			res.status(200).json(data)
-		})
-		.catch(error => {
-			// console.error("Error:", error)
-			res.status(500).json(error)
-		})
+	const res1 = await axios.post(
+		url,
+		{ name: "John Doe" },
+		{
+			headers: {
+				"content-type": "text/json",
+				"Api-Key": "4NKQ3-815C2-8T5Q2-16318-55301",
+			},
+		}
+	)
+	console.log({ res1 })
+	// fetch(url, {
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Api-Key": "4NKQ3-815C2-8T5Q2-16318-55301",
+	// 	},
+	// })
+	// 	.then(response => response.json())
+	// 	.then(data => {
+	// 		res.status(200).json(data)
+	// 	})
+	// 	.catch(error => {
+	// 		// console.error("Error:", error)
+	// 		res.status(500).json(error)
+	// 	})
 })
 
 // Start the server
